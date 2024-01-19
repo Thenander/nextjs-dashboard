190d48a15c455894078383d6b7fcc2724ea25c52
Joacim Thenander <info@microlab.se>
2024-01-19 15:21:31 +0100

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Nunito } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+ 
+export const nunito = Nunito({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
